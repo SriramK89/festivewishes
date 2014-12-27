b4593d897fc0363ea246a0a91fa465269adf7724
Sriram Kumar <ksrirambecse@gmail.com>
2014-12-27 11:26:57 +0530

    FestiveWishes - Completed the wishes for all festivals
    * There are 18 festivals as of now.

--- config/initializers/festive_wishes_constants.rb
+++ config/initializers/festive_wishes_constants.rb
@@ -7,14 +7,19 @@ module FestiveWishes
     HOLI = "holi"
     TELUGU_NEW_YEAR = "telugu_new_year"
     TAMIL_NEW_YEAR = "tamil_new_year"
+    EASTER_SUNDAY = "easter_sunday"
     LABOUR_DAY = "labour_day"
     INDEPENDENCE_DAY = "independence_day"
     RAKSHA_BANDHAN = "raksha_bandhan"
+    ONAM = "onam"
     GANESH_CHATHURTHI = "ganesh_chathurthi"
+    BAKRID = "bakrid"
     DUSSEHRA = "dussehra"
     DIWALI = "diwali"
+    RAMZAN = "ramzan"
     CHRISTMAS = "christmas"
-    ALL = [NEW_YEAR, PONGAL, REPUBLIC_DAY, VALENTINES_DAY, HOLI, TELUGU_NEW_YEAR, TAMIL_NEW_YEAR, LABOUR_DAY,
-      INDEPENDENCE_DAY, RAKSHA_BANDHAN, GANESH_CHATHURTHI, DUSSEHRA, DIWALI, CHRISTMAS]
+    ALL = [NEW_YEAR, PONGAL, REPUBLIC_DAY, VALENTINES_DAY, HOLI, TELUGU_NEW_YEAR, TAMIL_NEW_YEAR, EASTER_SUNDAY,
+      LABOUR_DAY, INDEPENDENCE_DAY, RAKSHA_BANDHAN, ONAM, GANESH_CHATHURTHI, BAKRID, DUSSEHRA, DIWALI, RAMZAN,
+      CHRISTMAS]
   end
 end
